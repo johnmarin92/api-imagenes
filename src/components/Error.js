4bfcb1e7a4ae93a7a42648d6f35570cf52a7853a
johnmarin92 <bxeneixe@gmail.co>
2021-03-12 10:43:02 -0300

    first commit

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Error = ({mensaje}) => {
+    return ( 
+         <p className="my-3 p-4 text-center alert alert-primary">{mensaje}</p>
+     );
+}
+
+Error.propTypes = {
+    mensaje: PropTypes.string.isRequired
+}
+ 
+export default Error;
